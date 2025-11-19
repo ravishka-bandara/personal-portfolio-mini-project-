8addcfe6796bf08f302461a09806d8c2d32b8fe1
ravishka-bandara <ravishkabandara01@gmail.com>
2025-11-19 20:46:44 +0530

    feat: add hamburger menu functionalities using js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function scrolltoprojects(){
         behavior: 'smooth'
     });
 }
+
+
+//hamb urger menu functionalities
+
+const hamburger=document.querySelector('.hamburger');
+const navMenu=document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+//closing the menu click on a link
+
+document.querySelectorAll('.nav-menu a').forEach(link =>{
+    link.addEventListener('click',()=>{
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
